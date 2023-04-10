0c4377466f737718db64aeee4f670d65985a8eea
RMvirtual <rmvirtual@AOL.com>
2023-04-10 09:40:13 +0100

    Moves triangle to right side of screen.

--- fortesque.cpp
+++ fortesque.cpp
@@ -10,16 +10,23 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-int main()
+
+void initialiseGLFW()
 {
-    // glfw: initialize and configure
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
 
-    // glfw window creation
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+
+int main()
+{
+    initialiseGLFW();
+
+    GLFWwindow* window = glfwCreateWindow(
+        SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -33,12 +40,12 @@ int main()
     // glad: load all OpenGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
+
         return -1;
     }
 
     // Build and compile our shader program.
-    // You can name your shader files however you like.
-    Shader ourShader("shader.vs", "shader.fs"); 
+    Shader ourShader("shader.vs", "shader.fs");    
 
     // Set up vertex data (and buffer(s)) and configure vertex 
     // attributes.
@@ -52,16 +59,19 @@ int main()
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    
+    // Bind the Vertex Array Object first, then bind and set vertex
+    // buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // position attribute
+    // Position attribute.
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // color attribute
+
+    // Color attribute.
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
@@ -72,22 +82,24 @@ int main()
     // necessary.
     // glBindVertexArray(0);
 
-
     // render loop
     while (!glfwWindowShouldClose(window)) {
-        // input
         processInput(window);
 
-        // render
+        // Render.
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // render the triangle
+        // Render the triangle.
+
         ourShader.use();
+        float offset = 0.5f;
+        ourShader.setFloat("xOffset", offset);
+
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // GLFW: swap buffers and poll IO events.
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -98,20 +110,27 @@ int main()
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     glfwTerminate();
+
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+
+/**
+Process all input : query GLFW whether relevant keys are pressed
+/released this frame and react accordingly 
+*/
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+
+// GLFW window size change callback.
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
+    // make sure the viewport matches the new window dimensions; note
+    // that width and  height will be significantly larger than
+    // specified on retina displays.
     glViewport(0, 0, width, height);
 }
