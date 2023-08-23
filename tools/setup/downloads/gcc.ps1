49ff40ec860f0c31e3b7d05b12e9f89018cf5917
RMvirtual <rmvirtual@AOL.com>
2023-08-23 22:13:58 +0100

    Remove zip files after finished.

--- tools/setup/downloads/gcc.ps1
+++ tools/setup/downloads/gcc.ps1
@@ -1,4 +1,12 @@
-$url = "https://github.com/brechtsanders/winlibs_mingw/releases/download/13.2.0-16.0.6-11.0.0-ucrt-r1/winlibs-x86_64-posix-seh-gcc-13.2.0-llvm-16.0.6-mingw-w64ucrt-11.0.0-r1.zip"
+$TARGET = "$env:DEVENV\devenv"
+$ZIP_FILE = "$env:DEVENV\mingw64.zip"
 
-Invoke-WebRequest -Uri $url -OutFile "$outputFolder\compiler.zip"
-Expand-Archive -Path "$outputFolder\compiler.zip" -DestinationPath "$env:DEVENV\devenv"
+$URL = "https://github.com/brechtsanders/winlibs_mingw/" `
+    + "releases/download/13.2.0-16.0.6-11.0.0-ucrt-r1/" `
+    + "winlibs-x86_64-posix-seh-gcc" `
+    + "-13.2.0-llvm-16.0.6-mingw-w64ucrt-11.0.0-r1.zip"
+
+Invoke-WebRequest -Uri $URL -OutFile $ZIP_FILE
+Expand-Archive -Path $ZIP_FILE -DestinationPath $TARGET
+Remove-Item $ZIP_FILE > $null
+
--- tools/setup/downloads/glfw.ps1
+++ tools/setup/downloads/glfw.ps1
@@ -1,10 +1,10 @@
 $TARGET = "$env:DEVENV\devenv"
-
+$ZIP_FILE = "$TARGET\glfw.zip"
 
 $version = "glfw-3.3.8.bin.WIN64"
 $url = "https://github.com/glfw/glfw/releases/download/3.3.8/$version.zip"
 
 
-Invoke-WebRequest -Uri $url -OutFile "$TARGET\glfw.zip" > $null
-Expand-Archive -Path "$TARGET\glfw.zip" -DestinationPath $TARGET > $null
+Invoke-WebRequest -Uri $url -OutFile $ZIP_FILE > $null
+Expand-Archive -Path $ZIP_FILE -DestinationPath $TARGET > $null
 Rename-Item "$TARGET\$version" "$TARGET\glfw" > $null
