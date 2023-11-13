f452e5c827897c58322fc3e5d59d970909aa6d86
RMvirtual <rmvirtual@AOL.com>
2023-11-13 20:29:59 +0000

    Red square using shader GLSL lang.

--- src/fortesque.cpp
+++ src/fortesque.cpp
@@ -9,16 +9,19 @@ const unsigned int SCREEN_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vertexColor;\n"
     "}\n\0";
 
 
@@ -123,7 +126,6 @@ int main()
     glBufferData(
         GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // End WIP here.
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -189,6 +191,7 @@ int main()
 
     // glfw terminate, clearing all previously allocated GLFW resources.
     glfwTerminate();
+
     return 0;
 }
 
