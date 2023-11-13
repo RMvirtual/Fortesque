446cd7d928ebc6717a9e38b47e7ba5904f42c2d0
RMvirtual <rmvirtual@AOL.com>
2023-11-13 20:56:47 +0000

    Rainbow triangle.

--- src/fortesque.cpp
+++ src/fortesque.cpp
@@ -7,21 +7,22 @@
 const unsigned int SCREEN_WIDTH = 800;
 const unsigned int SCREEN_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource ="#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "out vec4 vertexColor;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
-    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+    "   ourColor = aColor;\n"
     "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
+    "in vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = ourColor;\n"
+    "   FragColor = vec4(ourColor, 1.0f);\n"
     "}\n\0";
 
 
@@ -105,26 +106,12 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Draw square with index-based approach.
     float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };
-
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    
-    glBufferData(
-        GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        // positions         // colors
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    }; 
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -133,19 +120,27 @@ int main()
     // Bind vertex array object first, then bind and set vertex 
     // buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // Position attribute.
     glVertexAttribPointer(
-        0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+        0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) 0);
     
     glEnableVertexAttribArray(0);
+
+    // Colour attribute.
+    glVertexAttribPointer(
+        1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+        (void*) (3 * sizeof(float))
+    );
+
+    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer 
     // registered VBO as the vertex attribute's bound vertex buffer 
     // object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    // glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
     // You can unbind the VAO afterwards so other VAO calls won't 
     // accidentally modify this VAO, but this rarely happens.
@@ -169,13 +164,6 @@ int main()
         // Draw our first triangle
         glUseProgram(shaderProgram);
         
-        // Update uniform colour.
-        float timeValue = glfwGetTime();
-        float greenIntensity = sin(timeValue) / 2.0f + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(
-            shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenIntensity, 0.0f, 1.0f);
-
         // Seeing as we only have a single VAO there's no need to bind
         // it every time, but we'll do so to keep things a bit more
         // organized.
@@ -183,8 +171,6 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
         // Hmm...
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time 
  
         // glfw: swap buffers and poll IO events.
@@ -195,7 +181,6 @@ int main()
     // optional: de-allocate all resources once outlived purpose.
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     // glfw terminate, clearing all previously allocated GLFW resources.
