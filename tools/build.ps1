9a2f86c2df1bb2e07e3ebc675f1324605b701a2c
RMvirtual <rmvirtual@AOL.com>
2024-02-03 17:19:52 +0000

    Separate debug version again.

--- tools/build.ps1
+++ tools/build.ps1
@@ -1,5 +1,6 @@
 $BUILD = "$env:DEVENV\build"
 $RELEASE = "$BUILD\release"
+$DEBUG = "$BUILD\debug"
 $TESTS = "$BUILD\tests"
 
 $SRC = "$env:DEVENV\src"
@@ -9,29 +10,52 @@ $RESOURCES = "$env:DEVENV\resources"
 $TEST_SRC = "$env:DEVENV\tests"
 
 
+function Compile-Fortesque
+{
+    param ([string] $destFolder, [bool] $debuggable = $false)
+
+    New-Item $destFolder -ItemType Directory > $null
+
+    $GLAD = "$DEV_LIBS\glad"
+    $GLFW = "$DEV_LIBS\glfw"
+    
+    if ($debuggable) {
+        g++.exe -o "$destFolder\main.exe" -g `
+            "$SRC\fortesque.cpp" "$SRC\shader.cpp" "$SRC\stb_image.cpp" `
+            "$GLAD\src\glad.c" `
+            -I"$GLAD\include" -I"$GLFW\include" -I"$SRC" -I"$LIB" `
+            -L"$GLFW\lib-mingw-w64" `
+            -lglfw3 -ldl -lgdi32 -luser32 `
+            -mwindows -std=c++17
+    }
+
+    else {
+        g++.exe -o "$destFolder\main.exe" `
+            "$SRC\fortesque.cpp" "$SRC\shader.cpp" "$SRC\stb_image.cpp" `
+            "$GLAD\src\glad.c" `
+            -I"$GLAD\include" -I"$GLFW\include" -I"$SRC" -I"$LIB" `
+            -L"$GLFW\lib-mingw-w64" `
+            -lglfw3 -ldl -lgdi32 -luser32 `
+            -mwindows -std=c++17
+    }
+
+    # Shaders
+    Copy-Item "$SRC\3.3.shader.fs" $destFolder
+    Copy-Item "$SRC\3.3.shader.vs" $destFolder
+
+    Copy-Item $RESOURCES $destFolder -Recurse
+}
+
+
 if (-Not (Test-Path $BUILD)) {New-Item $BUILD -ItemType Directory > $null}
 
 # Release.
 if (Test-Path $RELEASE) {Remove-Item $RELEASE -Recurse -Force > $null}
-New-Item $RELEASE -ItemType Directory > $null
+Compile-Fortesque $RELEASE
 
-$GLAD = "$DEV_LIBS\glad"
-$GLFW = "$DEV_LIBS\glfw"
-
-g++.exe -o "$RELEASE\main.exe" -g `
-    "$SRC\fortesque.cpp" "$SRC\shader.cpp" "$SRC\stb_image.cpp" `
-    "$GLAD\src\glad.c" `
-    -I"$GLAD\include" -I"$GLFW\include" -I"$SRC" -I"$LIB" `
-    -L"$GLFW\lib-mingw-w64" `
-    -lglfw3 -ldl -lgdi32 -luser32 `
-    -mwindows -std=c++17
-
-# Shaders
-Copy-Item "$SRC\3.3.shader.fs" $RELEASE
-Copy-Item "$SRC\3.3.shader.vs" $RELEASE
-
-# Resources
-Copy-Item $RESOURCES $RELEASE -Recurse
+# Debug.
+if (Test-Path $DEBUG) {Remove-Item $DEBUG -Recurse -Force > $null}
+Compile-Fortesque $DEBUG -debuggable $true
 
 <#
 # Tests.
