d10633bf2fc7cab95b25978aa587533bc7b6a3ad
RMvirtual <rmvirtual@AOL.com>
2024-02-03 17:44:44 +0000

    Cleaner build process.

--- tools/build.ps1
+++ tools/build.ps1
@@ -12,49 +12,41 @@ $TEST_SRC = "$env:DEVENV\tests"
 
 function Compile-Fortesque
 {
-    param ([string] $destFolder, [bool] $debuggable = $false)
+    param ([string] $destPath, [bool] $debuggable = $false)
 
-    New-Item $destFolder -ItemType Directory > $null
+    if (Test-Path $destPath) {Remove-Item $destPath -Recurse -Force > $null}
+    New-Item $destPath -ItemType Directory > $null
 
     $GLAD = "$DEV_LIBS\glad"
     $GLFW = "$DEV_LIBS\glfw"
+       
+    $srcFiles = -join(
+        "$SRC\fortesque.cpp $SRC\shader.cpp $SRC\stb_image.cpp ",
+        "$GLAD\src\glad.c"
+    )
+        
+    $includes = "-I$GLAD\include -I$GLFW\include -I$SRC -I$LIB"
+    $libraries = "-L$GLFW\lib-mingw-w64 -lglfw3 -ldl -lgdi32 -luser32" 
+    $platform = "-mwindows -std=c++17"
+
+    $compileOptions = "-o $destPath\main.exe "
+
+    if ($debuggable) {$compileOptions += "-g "}
+    $compileOptions += "$srcFiles $includes $libraries $platform"
     
-    if ($debuggable) {
-        g++.exe -o "$destFolder\main.exe" -g `
-            "$SRC\fortesque.cpp" "$SRC\shader.cpp" "$SRC\stb_image.cpp" `
-            "$GLAD\src\glad.c" `
-            -I"$GLAD\include" -I"$GLFW\include" -I"$SRC" -I"$LIB" `
-            -L"$GLFW\lib-mingw-w64" `
-            -lglfw3 -ldl -lgdi32 -luser32 `
-            -mwindows -std=c++17
-    }
-
-    else {
-        g++.exe -o "$destFolder\main.exe" `
-            "$SRC\fortesque.cpp" "$SRC\shader.cpp" "$SRC\stb_image.cpp" `
-            "$GLAD\src\glad.c" `
-            -I"$GLAD\include" -I"$GLFW\include" -I"$SRC" -I"$LIB" `
-            -L"$GLFW\lib-mingw-w64" `
-            -lglfw3 -ldl -lgdi32 -luser32 `
-            -mwindows -std=c++17
-    }
-
+    Start-Process g++.exe -ArgumentList $compileOptions -NoNewWindow -Wait
+    
     # Shaders
-    Copy-Item "$SRC\3.3.shader.fs" $destFolder
-    Copy-Item "$SRC\3.3.shader.vs" $destFolder
-
-    Copy-Item $RESOURCES $destFolder -Recurse
+    Copy-Item "$SRC\3.3.shader.fs" $destPath
+    Copy-Item "$SRC\3.3.shader.vs" $destPath
+    
+    Copy-Item $RESOURCES $destPath -Recurse
 }
 
 
 if (-Not (Test-Path $BUILD)) {New-Item $BUILD -ItemType Directory > $null}
 
-# Release.
-if (Test-Path $RELEASE) {Remove-Item $RELEASE -Recurse -Force > $null}
 Compile-Fortesque $RELEASE
-
-# Debug.
-if (Test-Path $DEBUG) {Remove-Item $DEBUG -Recurse -Force > $null}
 Compile-Fortesque $DEBUG -debuggable $true
 
 <#
