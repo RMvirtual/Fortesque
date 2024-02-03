c51e7fed6fb3d574d80d79a5ed022072fde800e2
RMvirtual <rmvirtual@AOL.com>
2024-02-03 20:26:40 +0000

    Refactored.

--- src/fortesque.cpp
+++ src/fortesque.cpp
@@ -168,20 +168,24 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        // Initialised as identity matrix.
+        glm::mat4 identityMatrix = glm::mat4(1.0f);
         
-        transform = glm::translate(
-            transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        glm::mat4 translatedMatrix = glm::translate(
+            identityMatrix, glm::vec3(0.5f, -0.5f, 0.0f));
 
-        transform = glm::rotate(
-            transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));      
+        glm::mat4 rotatedMatrix = glm::rotate(
+            translatedMatrix, 
+            (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f)
+        );
 
         ourShader.use();
+        
         unsigned int transformLoc = glGetUniformLocation(
             ourShader.ID, "transform");
         
         glUniformMatrix4fv(
-            transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+            transformLoc, 1, GL_FALSE, glm::value_ptr(rotatedMatrix));
 
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
