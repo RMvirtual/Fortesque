7125d04ec612af732713ac89813b3b19d34eb131
RMvirtual <rmvirtual@AOL.com>
2024-02-04 19:45:31 +0000

    Multiple cubes.

--- src/fortesque.cpp
+++ src/fortesque.cpp
@@ -99,6 +99,20 @@ int main()
         1, 2, 3  // Second triangle
     };
 
+    // World space positions of cubes to render.
+    glm::vec3 cubePositions[] {
+        glm::vec3(0.0f, 0.0f, 0.0f), 
+        glm::vec3(2.0f, 5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3(2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f, 3.0f, -7.5f),  
+        glm::vec3(1.3f, -2.0f, -2.5f),  
+        glm::vec3(1.5f, 2.0f, -2.5f), 
+        glm::vec3(1.5f, 0.2f, -1.5f), 
+        glm::vec3(-1.3f, 1.0f, -1.5f)  
+    };
+
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -213,49 +227,41 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        // 1st container.
-        glm::mat4 matrix {1.0f};  // Initalise as identity matrix.
-        auto translated = glm::translate(matrix, glm::vec3(0.5f, -0.5f, 0.0f));
-
-        auto rotated = glm::rotate(
-            translated, (float) glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-
         shader.use();
-        
-        glm::mat4 model {1.0f};
+
         glm::mat4 view {1.0f};
         glm::mat4 projection {1.0f};
 
-        model = glm::rotate(
-            model,
-            (float) glfwGetTime() * glm::radians(50.0f), 
-            glm::vec3(0.5f, 1.0f, 0.0f)
-        );  
-        
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        
         projection = glm::perspective(
             glm::radians(45.0f), 
             (float) SCREEN_WIDTH / (float) SCREEN_HEIGHT, 
-            0.1f, 
+            0.1f,
             100.0f
         );
 
-        unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
-        unsigned int viewLoc = glGetUniformLocation(shader.ID, "view");
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-        
         // Note: currently we set the projection matrix each frame, but 
         // since the projection matrix rarely changes it's often best 
         // practice to set it outside the main loop only once.
         shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
 
         glBindVertexArray(VAO);
         
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i=0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            
+            model = glm::rotate(
+                model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            
+            shader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
